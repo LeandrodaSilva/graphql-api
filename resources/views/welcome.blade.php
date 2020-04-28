bfb2b46d85d27b1ad688a8bdd9e8ede04b536596
LeandrodaSilva <ld_silva13@hotmail.com>
2020-04-27 22:25:36 -0300

    Add APP_NAME constant to views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Leandro da Silva</title>
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -94,7 +94,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Leandro da Silva
+                    {{ config('app.name', 'Laravel') }}
                 </div>
 
                 <div class="links">
